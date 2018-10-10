934ef0b0a8cc57cd642d01eb5ed056e8e0e6a358
Alex Pro <alex78pro@gmail.com>
2018-10-10 12:54:54 +0300

    Initial commit

---
+++ Example/MultyCoreLibrary/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MultyCoreLibrary
+//
+//  Created by alex78pro on 10/10/2018.
+//  Copyright (c) 2018 alex78pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
